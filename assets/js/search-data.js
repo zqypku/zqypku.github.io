8c45a8f76b9ea321e35ea95daa7b6f297e3c1ff2
zqypku <71053864+zqypku@users.noreply.github.com>
2026-01-18 05:42:47 +0000

    Deploying to gh-pages from @ zqypku/zqypku.github.io@f71f3349d13476ff9202c37d6c03d654592c0121 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -415,7 +415,12 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-titled-peering-behind-the-shield-guardrail-identification-in-large-language-models-was-accepted-by-aaai-2026-aics-workshop-and-aaai-2026-trustagent-workshop",
           title: 'Our paper titled Peering Behind the Shield: Guardrail Identification in Large Language Models...',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-peering-behind-the-shield-guardrail-identification-in-large-language-models",
+          title: 'Peering Behind the Shield: Guardrail Identification in Large Language Models',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_guard/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
@@ -460,6 +465,46 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/archive/9_project/";
             },},{
         id: 'social-dblp',
         title: 'DBLP',
